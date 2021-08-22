c6bf42e2d7a5a5228a0e2e2f75afcdedfa85f3b0
mi-kubota <sakura2020coordinator@gmail.com>
2021-08-22 19:34:28 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+jQuery(function () {
+  $('.main-nav-item').children("li").hover
+      (function() {
+          $(this).children("ul").stop().slideToggle("");
+    });
+});
+
+jQuery( function( $ ) {   
+
+  $( ".p-hamburger" ).on( "click", function() {         
+  $( '.p-hamburger,.main-nav' ).toggleClass( "open" ); 
+  $( "li" ).stop().slideDown( "" );   
+ 
+  } ); 
+  } );
+
+  
+  $(function(){
+    $(".btn-a").on("click", function() {
+        $(this).toggleClass('btn-b');
+        $( "li" ).stop().slideDown( "" );
+        $( this ).on( "click", function() { 
+          $( '.submenu li').stop().slideToggle( "" );
+        });
+    });
+});
+
+
+
+  
